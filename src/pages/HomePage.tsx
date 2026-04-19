612e30c5819e5e9f85f17a98de8e812545713d77
Dan Kennedy <dannykennedy@email.com>
2026-04-19 13:46:55 +1000

    feat: a little info on the home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,7 +5,17 @@ export default function HomePage() {
   return (
     <div>
       <h1>💖 Hello!</h1>
-      <p>Welcome to the uploader.</p>
+      <p>Archivist is your archive manager.</p>
+      <p>Here you can: </p>
+      <ul>
+        <li>Browse the items in your archive folder</li>
+        <li>Add metadata to these items</li>
+        <li>
+          Upload these items to a web archive for easy browsing (Coming soon!)
+        </li>
+      </ul>
+      <h2>How it works</h2>
+      <p></p>
       <p>
         <Link to="/browser">File Browser</Link>
       </p>
