eaa81b3331232f06d1e0f6b29ba307c936864ae7
Dan Kennedy <dannykennedy@email.com>
2026-04-26 14:21:53 +1000

    build: publish

--- forge.config.ts
+++ forge.config.ts
@@ -6,6 +6,7 @@ import { MakerRpm } from '@electron-forge/maker-rpm';
 import { VitePlugin } from '@electron-forge/plugin-vite';
 import { FusesPlugin } from '@electron-forge/plugin-fuses';
 import { FuseV1Options, FuseVersion } from '@electron/fuses';
+import { PublisherGithub } from '@electron-forge/publisher-github';
 
 const config: ForgeConfig = {
   packagerConfig: {
@@ -13,6 +14,12 @@ const config: ForgeConfig = {
     icon: 'src/icons/logo',
   },
   rebuildConfig: {},
+  publishers: [
+    new PublisherGithub({
+      repository: { owner: 'elsewheredreams', name: 'archive-uploader' },
+      prerelease: true,
+    }),
+  ],
   makers: [
     new MakerSquirrel({}),
     new MakerZIP({}, ['darwin']),
